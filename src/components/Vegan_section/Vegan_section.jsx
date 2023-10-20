e2d9c90bb318652aacb369e3092c9be134d06dbe
Vinan <80715419+AnnaTrojanowska@users.noreply.github.com>
2023-10-20 12:44:24 +0200

    add vegan section

---
+++ src/components/Vegan_section/Vegan_section.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./vegan_section.css"
+
+const Vegan_section = () => {
+  return (
+    <div className='bg_area'>
+        <div className='container_vegan'>
+            <div className='row_vegan'>
+                <div className='column_vegan'>
+                    <div className='content_vegan'>
+                        <h2>Przepisy Vege</h2>
+                        <p>Fusce nec ante vitae lacus aliquet vulputate. Donec scelerisque accumsan molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras sed accumsan neque. Ut vulputate, lectus vel aliquam congue, risus leo elementum nibh</p>
+                        <a href='#' className='btn delicious-btn'>Zobacz więcej przepisów</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Vegan_section
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Slider from '../components/Slider/Slider'
 import Top_category from '../components/Top_category/Top_category.jsx'
 import Top_receipes from '../components/Top_receipies/Top_receipes.jsx'
+import Vegan_section from '../components/Vegan_section/Vegan_section.jsx'
 
 const Home = () => {
   return (<>
     <Slider/>
     <Top_category/>
     <Top_receipes/>
+    <Vegan_section/>
     </>
   )
 }
