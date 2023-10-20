c0eb6dc02245f6c0c55766b23e4e2c5a32130241
Vinan <80715419+AnnaTrojanowska@users.noreply.github.com>
2023-10-20 12:03:31 +0200

    fix to routers

--- src/router/Routers.js
+++ src/router/Routers.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import {Route, Routes, Navigate} from 'react-router-dom'
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
 import Home from './../pages/Home'
 
 const Routers = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Navigate to='/home'/>}/>
-      <Route path='/home' element={<Home/>}/>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to='/home'/>}/>
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
