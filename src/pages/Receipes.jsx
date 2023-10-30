2fbeef95acb3cb7ea3a613cdb098f0f3315b3512
Vinan <80715419+AnnaTrojanowska@users.noreply.github.com>
2023-10-30 21:36:20 +0100

    add reactstrap

--- src/pages/Receipes.jsx
+++ src/pages/Receipes.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {Container, Row, Col, Button} from 'reactstrap'
 
 const Receipes = () => {
   return (
-    <div>Receipes</div>
+    <Container>
+        <Row xs="4">
+            <Col className="bg-light border">
+            Column
+            </Col>
+            <Col className="bg-light border">
+            Column
+            </Col>
+            <Col className="bg-light border">
+            Column
+            </Col>
+            <Col className="bg-light border">
+            Column
+            </Col>
+            <Col className="bg-light border">
+            Column
+            </Col>
+            <Col className="bg-light border">
+            Column
+            </Col>
+        </Row>
+    </Container>
   )
 }
 
