55201609a27fbaca078300e201ae47541bd0c214
Vinan <80715419+AnnaTrojanowska@users.noreply.github.com>
2023-11-20 20:12:47 +0100

    add others cuisines

--- src/components/Recipes/Japanese_recipes/Japanese_recipes.jsx
+++ src/components/Recipes/Japanese_recipes/Japanese_recipes.jsx
@@ -1,242 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {CardGroup, Card, CardImg, CardBody, CardText, CardTitle, CardSubtitle, Button} from 'reactstrap'
 import './japanese_recipes.css'
 import '../../Card_recipes/card_recipes.css'
 
 const Japanese_recipes = () => {
-  return (<>
-    <CardGroup className='recipe_group'>
-  <Card className='recipes_card'>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Card title
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Card subtitle
-      </CardSubtitle>
-      <CardText>
-        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-  <Card className='recipes_card'>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Card title
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Card subtitle
-      </CardSubtitle>
-      <CardText>
-        This card has supporting text below as a natural lead-in to additional content.
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-  <Card className='recipes_card'>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Card title
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Card subtitle
-      </CardSubtitle>
-      <CardText>
-        This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-</CardGroup>
-<CardGroup  className='recipe_group'>
-  <Card className='recipes_card'>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Card title
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Card subtitle
-      </CardSubtitle>
-      <CardText>
-        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-  <Card className='recipes_card'>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Card title
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Card subtitle
-      </CardSubtitle>
-      <CardText>
-        This card has supporting text below as a natural lead-in to additional content.
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-  <Card className='recipes_card'>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Card title
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Card subtitle
-      </CardSubtitle>
-      <CardText>
-        This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-</CardGroup>
-<CardGroup className='recipe_group'>
-  <Card className='recipes_card'>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Card title
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Card subtitle
-      </CardSubtitle>
-      <CardText>
-        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-  <Card className='recipes_card'>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Card title
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Card subtitle
-      </CardSubtitle>
-      <CardText>
-        This card has supporting text below as a natural lead-in to additional content.
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-  <Card className='recipes_card'>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Card title
-      </CardTitle>
-      <CardSubtitle
-        className="mb-2 text-muted"
-        tag="h6"
-      >
-        Card subtitle
-      </CardSubtitle>
-      <CardText>
-        This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.
-      </CardText>
-      <Button>
-        Button
-      </Button>
-    </CardBody>
-  </Card>
-</CardGroup>
-<div className='card_div'></div>
+  const [recipes, setRecipes] = useState(null);
+  const apiKey = '8d02f508a08743e5a1daa7e0c08d51b9';
+
+  useEffect(() => {
+    // Wykonaj zapytanie do API Spoonacular po załadowaniu komponentu
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&number=9&cuisine=Japanese`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Zapisz wyniki zapytania do stanu komponentu
+        setRecipes(data.results);
+      })
+      .catch(error => {
+        console.error('Błąd podczas pobierania danych z API Spoonacular:', error);
+      });
+  }, []);
+
+  return ( <>
+    <h3 className='heading_recipe'>KUCHNIA JAPOŃSKA</h3>
+    <div>
+      <CardGroup className='recipe_group'>
+        {recipes &&
+          recipes.map(recipe => (
+            <Card className='recipes_card' key={recipe.id}>
+              <CardImg alt="Card image cap" src={recipe.image} top width="100%" />
+              <CardBody>
+                <CardTitle className='card_title' tag="h5">{recipe.title}</CardTitle>
+                <a href="#" class="button button_recipes">Szczegóły</a>
+              </CardBody>
+            </Card>
+          ))}
+      </CardGroup>
+      
+    </div>
+    <div className='card_div'></div>
+  
 </>
   )
 }
